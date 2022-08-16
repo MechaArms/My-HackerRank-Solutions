3ca3bab2766979bfc5477c80d5b385d085132c49
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-16 07:38:44 -0300

    Create Basic - Solve Me First.kt

---
+++ Problem Solving/Basic - Solve Me First.kt
@@ -0,0 +1,45 @@
+/*
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+a = 7
+b = 3
+Return 10.
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b
+
+Constraints
+1 <= a,b <= 1000
+
+Sample Input
+a = 2
+b = 3
+
+Sample Output
+5
+
+Explanation
+2 + 3 = 5
+
+*/
+
+import java.util.*
+
+fun solveMeFirst(a: Int, b: Int): Int = a + b
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+    val sum = solveMeFirst(num1, num2)
+    println(sum)
+}
