2c58d2448110463ad2941a1bc994b1d201c0c28c
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-22 08:55:50 -0300

    Repeating a String in staircaise format

---
+++ Problem Solving/Algorithms/Basic - Staircase.kt
@@ -0,0 +1,90 @@
+/*
+Staircase detail
+
+This is a staircase of size n = 4:
+
+   #
+  ##
+ ###
+####
+
+Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+
+Function Description
+
+Complete the staircase function in the editor below.
+
+staircase has the following parameter(s):
+
+int n: an integer
+
+Print
+
+Print a staircase as described above.
+
+Input Format
+
+A single integer, n, denoting the size of the staircase.
+
+Constraints
+
+0 < n <= 100 .
+
+Output Format
+
+Print a staircase of size n using # symbols and spaces.
+
+Note: The last line must have 0 spaces in it.
+
+Sample Input
+
+6 
+
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n = 6.
+*/
+
+//My Solution
+//=============
+
+fun staircase(n: Int): Unit {
+    var stair: String = "#"
+    var count: Int = n - 1
+    for (i in 1.. n){
+        var backspace = " ".repeat(count)
+        println(backspace + stair)
+        stair = stair + "#"
+        count --
+    }
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    staircase(n)
+}
+
+//Best Solution
+//=============
+
+for (i in 1..n) {
+        println(" ".repeat(n - i) + "#".repeat(i))
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    staircase(n)
+}
